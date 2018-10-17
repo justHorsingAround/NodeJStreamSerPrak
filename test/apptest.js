9ee6525351345438decc11883217361362b0f07f
Tamas Kiss <sauriaster@gmail.com>
2018-10-17 14:42:10 +0200

    test failing test

--- test/apptest.js
+++ test/apptest.js
@@ -5,7 +5,7 @@ const request = require("request");
 const port = "9999";
 
 describe("Server running test", function(){
-    it("Check if there is response with status 200", function(){
+    it("Check if there is response with status 200 from /echo endpoint", function(done){
         var url = "http://localhost:"+ port + "/echo";
         request(url, function(error, response, body) {
             expect(response.statusCode).to.equal(200);
